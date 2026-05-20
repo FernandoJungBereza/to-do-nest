518fdc3cec985482484a0f00bda5c8a380a975b2
FernandoJungBereza <fernandojung.b@gmail.com>
2026-05-20 11:09:48 -0300

    Enhance ToDoList controllers by adding output DTO types to API responses for improved type safety and clarity in data handling.

--- src/modules/to-do-list/use-cases/get-one-to-do-list-by-id/get-one-to-do-list-by-userId.controller.ts
+++ src/modules/to-do-list/use-cases/get-one-to-do-list-by-id/get-one-to-do-list-by-userId.controller.ts
@@ -1,19 +1,18 @@
 import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { OutputToDoListDto } from '../../dtos/output-to-do-list.dto';
 import { GetOneToDoListByIdUseCase } from './get-one-to-do-list-by-userId.use-case';
 
 @ApiTags('To-do Lists')
 @Controller('to-do-lists')
 export class GetOneToDoListByIdController {
-  constructor(
-    private readonly getOneToDoListByIdUseCase: GetOneToDoListByIdUseCase,
-  ) {}
+	constructor(private readonly getOneToDoListByIdUseCase: GetOneToDoListByIdUseCase) {}
 
-  @Get(':id')
-  @ApiOperation({ summary: 'Get a to-do list by ID' })
-  @ApiResponse({ status: 200, description: 'To-do list found' })
-  @ApiResponse({ status: 404, description: 'To-do list not found' })
-  async getOneToDoListById(@Param('id', ParseUUIDPipe) id: string) {
-    return await this.getOneToDoListByIdUseCase.execute(id);
-  }
+	@Get(':id')
+	@ApiOperation({ summary: 'Get a to-do list by ID' })
+	@ApiResponse({ status: 200, description: 'To-do list found', type: OutputToDoListDto })
+	@ApiResponse({ status: 404, description: 'To-do list not found' })
+	async getOneToDoListById(@Param('id', ParseUUIDPipe) id: string) {
+		return await this.getOneToDoListByIdUseCase.execute(id);
+	}
 }
--- src/modules/to-do-list/use-cases/get-paginated-to-do-list/get-paginated-to-do-list.controller.ts
+++ src/modules/to-do-list/use-cases/get-paginated-to-do-list/get-paginated-to-do-list.controller.ts
@@ -1,6 +1,8 @@
+import { OutputPaginatedDto } from '@/shared/dtos/output-paginated.dto';
 import { PaginationSearchDto } from '@/shared/dtos/pagination-search.dto';
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { OutputToDoListDto } from '../../dtos/output-to-do-list.dto';
 import { GetPaginatedToDoListUseCase } from './get-paginated-to-do-list.use-case';
 
 @ApiTags('To-do Lists')
@@ -10,7 +12,7 @@ export class GetPaginatedToDoListController {
 
 	@Get()
 	@ApiOperation({ summary: 'Get paginated to-do lists for a user' })
-	@ApiResponse({ status: 200, description: 'To-do lists found' })
+	@ApiResponse({ status: 200, description: 'To-do lists found', type: OutputPaginatedDto<OutputToDoListDto[]> })
 	@ApiResponse({ status: 404, description: 'To-do lists not found' })
 	async getPaginatedToDoList(@Query() paginationSearchDto: PaginationSearchDto) {
 		return await this.getPaginatedToDoListUseCase.execute(paginationSearchDto);
